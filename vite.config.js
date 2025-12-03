096dc410841daa867b1dc624863461853b14ce4a
Naswan Nashir <naswannashir.r@gmail.com>
2025-12-03 21:19:24 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ export default defineConfig({
   base: './', // Ensures relative paths are used in build
   build: {
     assetsDir: 'assets', // Puts assets in dist/assets
+    chunkSizeWarningLimit: 100000, // Increase limit to suppress warnings for large models
   }
 });
